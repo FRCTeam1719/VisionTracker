0a1cab3f11c3c7f0e7c93265eed53caacd3588c5
aaronjeline <aaroneline.github@gmail.com>
2015-01-24 15:25:26 -0500

    Raises contrast of image

--- src/com/team3019/VisionCode/YellowToteTracker.java
+++ src/com/team3019/VisionCode/YellowToteTracker.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
@@ -13,7 +13,6 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
@@ -47,7 +46,6 @@ public class YellowToteTracker{
 		//for grey tote
 		grey_Lower = new Scalar(48,60,35);
 		grey_higher = new Scalar(81,84,54);
-		System.out.println("Tables");
 		//NetworkTable.setClientMode();
 		//NetworkTable.setIPAddress("roborio-1719.local");
 		//table = NetworkTable.getTable("SmartDashboard");
@@ -55,7 +53,6 @@ public class YellowToteTracker{
 		//main loop of the program
 		
 		while(true){
-			System.out.println("WHILELOOP");
 			try {
 				
 				while(/*table.isConnected()*/  true){
@@ -72,197 +69,78 @@ public class YellowToteTracker{
 	public static void processImage(){
 		try {
 			System.out.println("processImage()");
+			
+			//NETWORKING
+			//Connect to the camera
 			//the url of the camera snapshot to save ##.## with your team number
 			//Url url = new URL("http://10.##.##.11/axis-cgi/jpg/image.cgi");
 			URL url = new URL("http://10.17.19.102/axis-cgi/jpg/image.cgi");
 			URLConnection uc = url.openConnection();
-			//saves the image to a file
+			//Save the frame we just got from the camera, and write it to a file
 			BufferedImage img = ImageIO.read((uc.getInputStream()));
 			ImageIO.write(img, "png", new File("frame.png"));
-			//time for the OpenCV fun!
+			//PROCESSING
 			//Create mats, which are basically matrixes for storing images
 			Mat frame = new Mat();
-			Mat grey = new Mat();
 			Mat original = new Mat();
 			//Load frame.png into a mat
 			original = Highgui.imread("frame.png");
-			ArrayList<MatOfPoint> contours = new ArrayList<>();
+			
+			//Raise the contrast
+			frame = raiseContrast(original);
+			
+			
+			/*Not currently in use
 			//Drop everything that isn't green
-			Core.inRange(original, thresh_Lower, thresh_Higher, frame);
-			/*
-			//find the cluster of particles
-			Imgproc.findContours(frame, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-			Imgproc.cvtColor(frame, frame, Imgproc.COLOR_GRAY2BGR);
-			//iterating through the list of contours and removing the ones with an "area" less then 100
-			for (Iterator<MatOfPoint> iterator = contours.iterator(); iterator.hasNext();) {
-				MatOfPoint matOfPoint = (MatOfPoint) iterator.next();
-				if(matOfPoint.width() * matOfPoint.height() < 20){
-					iterator.remove();
-				}	
-			}
-			//if theres only one contour dont do the silly math of the bounding rectangles
-			if(contours.size() == 1){
-					Rect rec1 = Imgproc.boundingRect(contours.get(0));
-					Core.rectangle(frame, rec1.tl(), rec1.br(), Yellow);
-					String string = "TargetFound at X:" + (rec1.tl().x + rec1.br().x) / 2 + "Y:" + (rec1.tl().y + rec1.br().y) / 2;
-					Core.putText(frame, string, new Point(200,frame.size().height-10), Core.FONT_HERSHEY_PLAIN, 1, Red);
-			}
-			//i wonder if there is two totes...
-			else if(contours.size() == 2 || contours.size() == 4){
-				ArrayList<Rect> target1 = new ArrayList<Rect>();
-				ArrayList<Rect> target2 = new ArrayList<Rect>();
-					target1.add(Imgproc.boundingRect(contours.get(0)));
-					target1.add(Imgproc.boundingRect(contours.get(1)));
-					if(contours.size() == 4){
-						target2.add(Imgproc.boundingRect(contours.get(2)));
-						target2.add(Imgproc.boundingRect(contours.get(3)));
-					}
-				Point tl = target1.get(0).tl();
-				Point br = target1.get(0).br();
-				//outer bounds of one rectangle
-				for(Rect rec : target1){
-					if(tl.x > rec.tl().x){
-						tl.x = rec.tl().x;
-					}
-					if(tl.y > rec.tl().y){
-						tl.y = rec.tl().y;
-					}
-					if(br.x < rec.br().x){
-						br.x = rec.br().x;
-					}
-					if(br.y < rec.br().y){
-						br.y = rec.br().y;
-					}
-				}
-				Rect bb1 = new Rect(tl, br);
-				Core.rectangle(frame, bb1.br(),bb1.tl(), Yellow);
-				Rect bb2 = null;
-				//the 4 L will make 4 contours
-				//if there was 4 contours then fill the outer bounds of the second rectangle
-				if(!target2.isEmpty()){
-					tl = target2.get(0).tl();
-					br = target2.get(0).br();
-					for(Rect rec : target2){
-						if(tl.x > rec.tl().x){
-							tl.x = rec.tl().x;
-						}
-						if(tl.y > rec.tl().y){
-							tl.y = rec.tl().y;
-						}
-						if(br.x < rec.br().x){
-							br.x = rec.br().x;
-						}
-						if(br.y < rec.br().y){
-							br.y = rec.br().y;
-						}
-					}
-					bb2 = new Rect(tl, br);
-				}
-				String string;
-				if(null != bb2){
-					Core.rectangle(frame, bb2.br(),bb2.tl(), Yellow);
-					//checking to see if the totes line up, because stacked totes line up!
-					if(Math.abs(bb1.x-bb2.x) < 5){
-						string = "Two totes stacked";
-						Core.line(frame, new Point(bb1.x+bb1.width/2,bb1.y+bb1.height/2),
-								new Point(bb2.x+bb2.width/2,bb2.y+bb2.height/2), Green);
-						//System.out.println(Math.abs((bb1.y + bb1.height/2) - (bb2.y + bb2.height)));
-					}
-					else{
-					string = "Two totes found";
-					}
-				}
-				else{
-					//knowing how far off the center of the image you can set your robot to align with it, essentially going towards it
-					string = "Off center by " + (bb1.x + bb1.width/2 - 320) + " pixels";
-					table.putNumber("Off Center", (bb1.x + bb1.width/2 - 320));
-					int print = bb1.width;
-					System.out.println(print / 640.0);
-					table.putNumber("ToteToScreen", bb1.width / 640.0);
-				}
-				//putting the data in a visible form
-				Core.putText(frame, string, new Point(200,frame.size().height-10), Core.FONT_HERSHEY_PLAIN, 1, Red);
-			}
-			else if(contours.size() == 3){
-				ArrayList<Rect> rect = new ArrayList<>();
-				for(MatOfPoint mOP : contours){
-					rect.add(Imgproc.boundingRect(mOP));
-					Rect temp = rect.get(rect.size()-1);
-					Core.rectangle(frame, temp.br(),temp.tl(), Red);
-				}
-				for(int i = 1; i < contours.size();i++){
-					if(Math.abs(rect.get(i).y - rect.get(i-1).y) < 5){
-						
-						Rect r1 = rect.get(i);
-						Rect r2 = rect.get(i-1);
-						Point tl = r1.tl();
-						Point br = r1.br();
-						if(tl.x > r2.tl().x){
-							tl.x = r2.tl().x;
-						}
-						if(tl.y > r2.tl().y){
-							tl.y = r2.tl().y;
-						}
-						if(br.x < r2.br().x){
-							br.x = r2.br().x;
-						}
-						if(br.y < r2.br().y){
-							br.y = r2.br().y;
-						}
-						break;
-					}
-				}
-			}
-			else{
-				
-				for(MatOfPoint mOP : contours){
-					Rect rec = Imgproc.boundingRect(mOP);
-					Core.rectangle(frame, rec.tl(), rec.br(), Red);
-				}
-			}
-			//processing for grey totes
-			Core.inRange(original, grey_Lower, grey_higher, grey);
-			Imgproc.findContours(grey, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-			Imgproc.cvtColor(grey, grey, Imgproc.COLOR_GRAY2BGR);
-			//iterating through the list of contours and removing the ones with an "area" less then 100
-			for (Iterator<MatOfPoint> iterator = contours.iterator(); iterator.hasNext();) {
-				MatOfPoint matOfPoint = (MatOfPoint) iterator.next();
-				if(matOfPoint.width() * matOfPoint.height() < 150){
-					iterator.remove();
-				}	
-			}
-			for(MatOfPoint mop : contours){
-				Rect bb = Imgproc.boundingRect(mop);
-				Core.rectangle(grey, bb.tl(), bb.br(), Red);
-			} */
-			//gui on the image
+			//Core.inRange(original, thresh_Lower, thresh_Higher, frame);
+			//Draw contours
+			//ArrayList<MatOfPoint> contours = new ArrayList<>();
+			*/
+			
+			//WRITING
+			//Put the crosshairs on the image
 			Core.line(frame, new Point(frame.width()/2,100),new Point(frame.width()/2,frame.height()-100), Blue);
 			Core.line(frame, new Point(150,frame.height()/2),new Point(frame.width()-150,frame.height()/2), Blue);
+			//Write text on the image
 			Core.putText(frame, "Team 1719", new Point(0,20), 
 					Core.FONT_HERSHEY_PLAIN, 1, Red);
-			//view this file to see the vision tracking
-			//windows will update the image after every save
-			//System.out.println("Writing image");
+			//Write the final mat to a file
 			Highgui.imwrite("rectangle.png", frame);
-			//System.out.println("Wrote Image");
-			//Highgui.imwrite("grey.jpg", grey);
 		//mostly for debugging but errors happen
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();	
 		}
 	}
-	//takes in the target width or length and a boolean and returns a distance
-	//assumes the use of the Axis M1013 camera
-	//depreciated because it needs further testing and does not work
-	@Deprecated
-	public static double getDistance(double targetInPixels,boolean width){
-		//double FOVpix = width ? 640 : 480;
-		double FOVpix = 480;
-		double FOVft = ((7 / 12.0) / targetInPixels) * FOVpix;
-		double returnd = FOVft / Math.tan(33.5);
-		return returnd;
+	
+	
+	public static Mat raiseContrast(Mat input){
+		//Create some mats
+		Mat ycrcb = input;
+		Mat result = input;
+		//Convert our input mat to the YCrCb colorspace
+		Imgproc.cvtColor(input, ycrcb, Imgproc.COLOR_BGR2YCrCb);
+		//Create and arraylist for storing the channels of the image
+		ArrayList<Mat> channels = new ArrayList<Mat>();
+		//Split our mat into the arrayList of channels
+		Core.split(ycrcb, channels);
+		//Convert the arrayList to an array
+		Mat[] channelArr = new Mat[channels.size()];
+		channelArr = channels.toArray(channelArr);
+		//The contrast command
+		Imgproc.equalizeHist(channelArr[0], channelArr[0]);
+		//Convert the array back to an arrayList
+		ArrayList<Mat> channelFinal = new ArrayList<Mat>(Arrays.asList(channelArr));
+		//Merge the channels back into the mat
+		Core.merge(channelFinal,ycrcb);
+		//Convert the mat back to the BGR colospace
+		Imgproc.cvtColor(ycrcb,result,Imgproc.COLOR_YCrCb2BGR);
+		return result;
 	}
+	
+	
+	
+	
 
 	
 	
